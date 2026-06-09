b3512886e2eb0bc151e6fe3d276d9a166c472574
Karan007-official <www.karanofficial111@gmail.com>
2026-06-09 11:56:02 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,8 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: process.env.DB_PASS,
+  password: "ENTER YOUR PASSWORD",
   database: "expense_tracker",
 });
 
